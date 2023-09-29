a29820d905868a0e89cf751b0e869cc6fcc79d21
MohammedMah5 <mohmedahmeduae1@gmail.com>
2023-09-29 11:18:55 +0400

    Commit inital files

---
+++ index.js
@@ -0,0 +1,26 @@
+let score = 0
+document.getElementById("hadd1").addEventListener('click' , ()=> {
+    score += 1
+    document.getElementById("score").textContent = score
+})
+document.getElementById("hadd2").addEventListener('click' , ()=> {
+    score += 2
+    document.getElementById("score").textContent = score
+})
+document.getElementById("hadd3").addEventListener('click' , ()=> {
+    score += 3
+    document.getElementById("score").textContent = score
+})
+let score2 = 0
+document.getElementById("gadd1").addEventListener('click' , ()=> {
+    score2 += 1
+    document.getElementById("score2").textContent = score2
+})
+document.getElementById("gadd2").addEventListener('click' , ()=> {
+    score2 += 2
+    document.getElementById("score2").textContent = score2
+})
+document.getElementById("gadd3").addEventListener('click' , ()=> {
+    score2 += 3
+    document.getElementById("score2").textContent = score2
+})
